e468900c8dc0d3bc4b1a11a526fb4df776c731dd
andysuyf <andysuyf@gmail.com>
2025-09-02 16:15:02 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,8 +1,53 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-    }
+    maotai: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
+        name: '茅台资源',
+    },
+    dbzy: {
+        api: 'https://dbzy.tv/api.php/provide/vod',
+        name: '豆瓣资源',
+    },
+    wolong: {
+        api: 'https://wolongzyw.com/api.php/provide/vod',
+        name: '卧龙资源',
+    }, 
+    modu: {
+        api: 'https://www.mdzyapi.com/api.php/provide/vod',
+        name: '魔都资源',
+    },
+    ruyi: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    },
+    maoyan: {
+        api: 'https://api.maoyanapi.top/api.php/provide/vod',
+        name: '猫眼资源',
+    },
+    jisu: {
+      "api": "https://jszyapi.com/api.php/provide/vod",
+      "name": "极速资源",
+      "detail": "https://jszyapi.com"
+    },
+    tyyszy: {
+      "api": "https://tyyszy.com/api.php/provide/vod",
+      "name": "天涯资源",
+    },
+    zuid: {
+      "api": "https://api.zuidapi.com/api.php/provide/vod",
+      "name": "最大资源",
+    },
+    wujin: {
+      "api": "https://api.wujinapi.me/api.php/provide/vod",
+      "name": "无尽资源",
+    },
+    bfzy: {
+      "api": "https://bfzyapi.com/api.php/provide/vod",
+      "name": "暴风资源",
+    },
+    wwdj: {
+      "api": "https://api.wwzy.tv/api.php/provide/vod",
+      "name": "旺旺短剧",
+    },
 };
 
 // 调用全局方法合并
